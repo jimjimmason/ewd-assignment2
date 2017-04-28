46c6f7efa8c61ca8360f511bdc36bde5d4ee6b36
jimjimmason <jmason@eircom.net>
2017-04-28 18:48:47 +0100

    Reviews router complete

--- api/reviews/index.js
+++ api/reviews/index.js
@@ -29,84 +29,56 @@ router.post('/', (req, res) => {
      
 });
 
-/*
-
-//Add a review
-router.post('/', (req, res) => {
-
-    const newReview = req.body;
-    let result = null;
-    if (newReview){
-         result = reviewsAPI.add(newReview.title, newReview.link);
-    }    
-    if (result>0){
-       return    res.status(201).send({message: "Review Created", id: result});
-      }
-        return   res.status(400).send({message: "Unable to find Review in request. No Review Found in body"});
-     
-});
 
 //upvote a review
-router.post('/:id/upvote', (req, res) => {
-   const id = req.params.id;
-   const result = reviewsAPI.upvote(id);
-             if (result) {
-                 
-                 return  res.status(200).send({message: `Review ${id} Upvoted`}); 
-                }
-            return    res.status(404).send({message: `Unable to find Review ${id}`});
-           
+router.post('/:id/upvotes', (req, res) => {
+  const id = req.params.id;
+  Review.findById(id, function (err, review) { 
+    if(err) { return handleError(res, err); }
+      review.upvotes++;
+      review.save(err => {
+        if (err) {return handleError(res, err);}
+        return res.status(201).send({review});
+    });
+  });
 });
 
-//get review
+//get a review
 router.get('/:id', (req, res) => {
-    const id = req.params.id;
-    const review = reviewsAPI.getReview(id);
-
-       if(review){
-               return   res.status(200).send(review);
-              }
-              return    res.status(404).send({message: `Unable to find Review ${id}`}); 
-     
+  const id = req.params.id;
+  Review.findById(id, function (err, review) { 
+    if(err) { return handleError(res, err); }
+    return res.send({review});
+  });       
 });
 
 //add comment
 router.post('/:id/comments', (req, res) => {
    const id = req.params.id;
    const comment = req.body;
-   console.log(id);
-   const result = reviewsAPI.addComment(id,comment.comment, comment.author);
- if (result){
-            return  res.status(200).send({message: `Comment added to Review ${id}`}); 
-      }
-            return   res.status(400).send({message: `Unable to add comment to Review ${id}`});
-            
+   Review.findById(id, (err, review)=>{ 
+        if(err) { return handleError(res, err); }
+        review.comments.push(comment);
+        review.save(err => {
+          if (err) {return handleError(res, err);}
+           return res.status(201).send({review});
+        });
+  });
 });
 
-//add comment
-router.get('/:id/comments', (req, res) => {
-   const id = req.params.id;
-   const comment = req.body;
-   console.log(id);
-   const result = reviewsAPI.getComments(id,comment.comment, comment.author);
- if (result){
-            return  res.status(200).send({message: `Comment added to Review ${id}`}); 
-      }
-            return   res.status(400).send({message: `Unable to add comment to Review ${id}`});
-            
-});
 
-//upvote comment
-router.post('/:reviewId/comments/:commentId/upvote', (req, res) => {
+//upvote a comment on a review
+router.post('/:postId/comments/:commentId/upvotes', (req, res) => {
    const commentId = req.params.commentId;
-   const reviewId = req.params.reviewId;
-   const result = reviewsAPI.upvoteComment(reviewId, commentId);
-             if (result) {
-                      return res.status(200).send({message: `Comment ${commentId} on Review ${reviewId} Upvoted`}); 
-                }
-            return res.status(404).send({message: `Unable to find comment ${commentId} on Review ${reviewId}`});
-             
+   const postId = req.params.postId;
+   Review.findById( postId, (err, review)=>{
+        if(err) { return handleError(res, err); }
+           review.comments.id(commentId).upvotes++;
+           review.save(err => {
+           if (err) {return handleError(res, err);}
+                return res.status(201).send({review});
+           });
+  });
 });
 
-*/
 export default router;
